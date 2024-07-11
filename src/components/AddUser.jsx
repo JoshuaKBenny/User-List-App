32b17cc8e5639208d0e987177d69c046c77840dc
JoshuaKBenny <joshuakbenny@gmail.com>
2024-07-11 13:12:34 +0530

    final

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddUser = () => {
+  return (
+    <div>
+        <Navbar/>
+      <div className="container">
+        <div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label"> Name</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">User Name</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Email</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Phone No:</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Website</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <center><button className="btn btn-success">Submit</button></center>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddUser
